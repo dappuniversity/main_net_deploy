346263d9e83a73afddded7daffd83af6eddddd24
Gregory McCubbin <gwmccubbin@gmail.com>
2018-09-14 09:26:53 -0500

    Adds main net deployment settings

--- truffle-config.js
+++ truffle-config.js
@@ -8,6 +8,18 @@ module.exports = {
       port: 8545,
       network_id: "*" // Match any network id
     },
+    mainnet: {
+      provider: function() {
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          `https://mainnet.infura.io/${process.env.INFURA_API_KEY}`
+        )
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      confirmations: 2,
+      network_id: 1
+    },
     kovan: {
       provider: function() {
         return new HDWalletProvider(
@@ -18,7 +30,7 @@ module.exports = {
       gas: 5000000,
       gasPrice: 25000000000,
       network_id: 42
-    },
+    }
   },
   solc: {
     optimizer: {
